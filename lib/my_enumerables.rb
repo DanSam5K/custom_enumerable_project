12f76af0ebd495e87a5bcb3b2f7dd71f1b576258
Daniel Samuel <dansam5000@gmail.com>
2022-11-13 03:16:48 +0100

    Implement custom enumerable my_each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,15 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    # Your code here
+    arr = self
+    i = 0
+    while i < arr.length
+      block_given? ? yield(arr[i]) : arr[i]
+      i += 1
+    end
+    arr
+  end
+    
 end
